cff1f3d58a49b023595dfb572cd93c097211519f
Sanzia7 <meritee@gmx.de>
2023-05-22 21:44:22 +0200

    writing the Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Footer />
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import s from './style.module.css'
 import logo from '../../media/logo.png'
 import { Link, NavLink } from 'react-router-dom'
+import cartImg from '../../media/shopbag.png'
+
 
 export default function Header() {
 
@@ -17,8 +19,11 @@ export default function Header() {
 
          <div className={s.block}>
             <nav className={s.nav}>
-               <NavLink>Main Page</NavLink>
+               <NavLink className={checkActivity} to='/'>Main Page</NavLink>
+               <NavLink className={checkActivity} to='/products/all'>All products</NavLink>
+               <NavLink className={checkActivity} to='/products/all/sales'>All sales</NavLink>
             </nav>
+            <Link to='/cart'><img src={cartImg} alt="shopbag" /></Link>
 
          </div>
 
