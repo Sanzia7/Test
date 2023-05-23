6f604b0050fef9b1bf4e9b6ca3e542a247e6f5d0
Sanzia7 <meritee@gmx.de>
2023-05-23 22:44:15 +0200

    Creating a SaleBanner component

---
+++ src/components/SaleBanner/SaleBanner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import s from './style.module.css'
+import gnome from '../../media/gnome.png'
+
+export default function SaleBanner() {
+
+
+   return (
+      <div className={s.wrapper}>
+         <img src={gnome} alt="gnome" />
+         <div>
+            <h2>
+               <div>5% off</div>
+               on the first order
+            </h2>
+            <form className={s.form}>
+               <input type="number" name='phone' placeholder='49' />
+               <button>Get a discount</button>
+            </form>
+         </div>
+      </div>
+   )
+}
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from '../../components/Home/Home'
+import SaleBanner from '../../components/SaleBanner/SaleBanner'
 
 export default function MainPage() {
    return (
       <div>
          <Home />
+         <SaleBanner />
       </div>
    )
 }
