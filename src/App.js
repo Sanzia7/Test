52ac4869ba79f297c487ed637c1c7e5de09cca58
Sanzia7 <meritee@gmx.de>
2023-05-30 19:22:00 +0200

    products reducers with serachproducts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css'
-import Header from './components/Header/Header'
-import MainPage from './pages/MainPage/MainPage';
-import CategoriesPage from './pages/CategoriesPage/CategoriesPage'
-import CartPage from './pages/CartPage/CartPage'
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage'
-import Footer from './components/Footer/Footer'
-import ProductsPage from './pages/ProductsPage/ProductsPage';
-import ProductInfoPage from './pages/ProductInfoPage/ProductInfoPage';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import MainPage from "./pages/MainPage/MainPage";
+import CategoriesPage from "./pages/CategoriesPage/CategoriesPage";
+import CartPage from "./pages/CartPage/CartPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Footer from "./components/Footer/Footer";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
+import ProductInfoPage from "./pages/ProductInfoPage/ProductInfoPage";
 
 // import { useDispatch } from 'react-redux';
 // import { useEffect } from 'react';
@@ -21,23 +21,23 @@ function App() {
   //   dispatch(fetchProductsList())
   // }, [])
 
-
   return (
-    <div>
+    <>
       <Header />
       <Routes>
-        <Route path='/' element={<MainPage />} />
-        <Route path='/categories/all' element={<CategoriesPage />} />
-        <Route path='/categories/:id' element={<ProductsPage />} />
-        <Route path='/products/all' element={<ProductsPage />} />
-        <Route path='/products/sale' element={<ProductsPage />} />
-        <Route path='/product/:id' element={<ProductInfoPage />} />
-        <Route path='/cart' element={<CartPage />} />
-        <Route path='/*' element={<NotFoundPage />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/products/all" element={<ProductsPage />} />
+        <Route path="/product/:id" element={<ProductInfoPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/products/:title" element={<ProductsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="/categories/:id" element={<ProductsPage />} />
       </Routes>
-      <Footer />      
-    </div>
+      <Footer />
+    </>
   );
 }
 
-export default App
+export default App;
--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -1,18 +1,65 @@
-const defaultState = []
+import { createSlice } from "@reduxjs/toolkit";
 
-const PRODUCTS_LOAD = 'PRODUCTS_LOAD'
+const productSlice = createSlice({
+   name: 'product',
+   initialState: {
+      products: [],
+   },
+   reducers: {
+      loadProducts: (state, action) => {
+         state.products = action.payload.map((product) => ({
+            ...product,
+            selectShow: true,
+            priceShow: true
+         }));
+      },
+      searchProducts: (state, action) => {
+         state.products = state.products.map((product) => ({
+            ...product,
+            selectShow: product.title
+               .toLowerCase()
+               .includes(action.payload.toLowerCase())
+         }))
+      },
+      resetSearchProducts: (state, action) => {
+         state.products = state.products.map((product) => ({
+            ...product,
+            selectShow: true,
+            priceShow: true,
+         }));
+      },
+      sortFilter: (state, action) => {
+         
+      }
 
-export const productsReducer = (state = defaultState, action) => {
-   switch (action.type) {
-      case PRODUCTS_LOAD: 
-         return [...action.payload]
-      
-      default:
-         return state
+
    }
-}
-
-export const productsLoadAction = (payload) => ({
-   type: PRODUCTS_LOAD,
-   payload
 })
+
+export default productSlice.reducer
+export const {
+   loadProducts,
+   searchProducts,
+   resetSearchProducts,
+} = productSlice.actions
+
+
+
+// const defaultState = []
+
+// const PRODUCTS_LOAD = 'PRODUCTS_LOAD'
+
+// export const productsReducer = (state = defaultState, action) => {
+//    switch (action.type) {
+//       case PRODUCTS_LOAD: 
+//          return [...action.payload]
+      
+//       default:
+//          return state
+//    }
+// }
+
+// export const productsLoadAction = (payload) => ({
+//    type: PRODUCTS_LOAD,
+//    payload
+// })
