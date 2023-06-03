095e45b60d50a0f6968da299a8176c5f86e7cbaf
Sanzia7 <meritee@gmx.de>
2023-06-03 19:01:12 +0200

    update component cartorder

---
+++ src/components/CartOrder/CartOrder.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import s from './style.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCartAction } from '../../store/reducers/cartReducer';
+import { fetchOrderSend } from '../../asyncActions/order';
+import ButtoGreen from '../UI/ButtonGreen/ButtonGreen'
+
+export default function CartOrder() {
+   const cart = useSelector(store => store.cart)
+   const dispatch = useDispatch()
+
+   const totalSum = cart.reduce( (prev, item) =>
+         prev + item.count * (item.discont_price
+            ? item.discont_price
+         : item.price), 0)
+   
+   const onSubmit = (e) => {
+      e.preventDefault()
+      fetchOrderSend(e.target.phone.value)
+      e.target.reset()
+      dispatch(clearCartAction())
+   }
+
+   return (
+      <div className={s.order}>
+         <h2>Order details:</h2>
+         <div>
+            <p>Total:</p>
+            <p>{totalSum} $</p>
+         </div>
+         <form onSubmit={onSubmit}>
+            <input type="tel"
+               name='phone'
+               placeholder='Phone number'
+               required
+            />
+            <ButtoGreen text="Order" />
+         </form>
+
+      </div>
+   )
+}
