5e55be28313ddea0d7b1b6ef400937bc6f87c38e
Sanzia7 <meritee@gmx.de>
2023-06-03 23:36:55 +0200

    updated notfoundpage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import s from './style.module.css'
+
 
 export default function NotFoundPage() {
+
    return (
-      <div>NotFoundPage</div>
+      <div className={s.container}>
+         <h1>SORRY!</h1>
+         <h2>SOMETHING IS WRONG...</h2>
+      </div>
    )
 }
