b02fdd6d08ed7ace33328bafcd58037baad72add
Sanzia7 <meritee@gmx.de>
2023-06-03 22:48:58 +0200

    update categoryitempage

--- src/pages/CategoryItemPage/CategoryItemPage.jsx
+++ src/pages/CategoryItemPage/CategoryItemPage.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import s from './style.module.css'
+import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCategory } from '../../asyncActions/products'
+import AllProducts from '../../components/AllProducts/AllProducts'
+
 
 export default function CategoryItemPage() {
+
+   const { id } = useParams()
+   const dispatch = useDispatch()
+
+   const category = useSelector(store => store.category)
+   useEffect(() => {
+      dispatch(fetchCategory(id))
+      window.scrollTo(0, 0)
+   }, [])
+
+   const title = category.data ? category.category.title : '';
+   const data = category.data ? category.data : []
+
+
    return (
-      <div>CategoryItemPage</div>
+      <div className={s.category_container}>
+         {
+            <AllProducts
+               products={data}
+               title={title}
+               filterShow={true}
+               saleShow={true}
+               location='category'
+            />
+         }
+      </div>
    )
 }
