de19de6bbdbc78f45e54177f67b8666747b3e93a
Sanzia7 <meritee@gmx.de>
2023-06-03 18:30:17 +0200

    update component cartshopping

--- src/components/CartShoping/CartShoping.jsx
+++ src/components/CartShoping/CartShoping.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import s from './style.module.css'
+import { useSelector } from 'react-redux'
+import Cart from '../Cart/Cart'
+
 
 export default function CartShoping() {
+
+   const cart = useSelector(store = store.cart)
+
    return (
-      <div>CartShoping</div>
+      <div className={s.shopping_cart}>
+         {
+            cart.map(item => 
+               <Cart
+                  key={item.id}
+                  {...item}
+               />)
+         }
+      </div>
    )
 }
