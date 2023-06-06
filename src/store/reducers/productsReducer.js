0908d39606e95f5db3734467c8f0dd2ed606b6a9
Sanzia7 <meritee@gmx.de>
2023-06-06 15:27:55 +0200

    update productsreducer

--- src/store/reducers/productsReducer.js
+++ src/store/reducers/productsReducer.js
@@ -3,14 +3,15 @@ const defaultState = {
    list: [],
 }
 
-const LOAD_PRODUCTS = 'LOAD_PRODUCTS'
-const LOAD_PRODUCTS_SALE = 'LOAD_PRODUCTS_SALE '
-const FILTER_SALE_PRODUCTS = 'FILTER_SALE_PRODUCTS'
-const SORT_DEFAULT_PRODUCTS = 'SORT_DEFAULT_PRODUCTS'
-const SORT_PRICE_DOWN_PRODUCTS = 'SORT_PRICE_DOWN_PRODUCTS'
-const SORT_PRICE_UP_PRODUCTS = 'SORT_PRICE_UP_PRODUCTS'
-const SORT_NAME_PRODUCTS = 'SORT_NAME_PRODUCTS'
-const FILTER_RANGE_PRODUCTS = 'FILTER_RANGE_PRODUCTS'
+const LOAD_PRODUCTS = '[PRODUCTS] LOAD_PRODUCTS'
+const LOAD_PRODUCTS_SALE = '[PRODUCTS] LOAD_PRODUCTS_SALE '
+const FILTER_SALE_PRODUCTS = '[PRODUCTS] FILTER_SALE_PRODUCTS'
+const FILTER_PRICE_RANGE = '[PRODUCTS] FILTER_PRICE_RANGE'
+const SORT_DEFAULT_PRODUCTS = '[PRODUCTS] SORT_DEFAULT_PRODUCTS'
+const SORT_PRICE_DOWN = '[PRODUCTS] SORT_PRICE_DOWN'
+const SORT_PRICE_UP = '[PRODUCTS] SORT_PRICE_UP'
+const SORT_NAME_PRODUCTS = '[PRODUCTS] SORT_NAME_PRODUCTS'
+
 
 
 export const productsReducer = (state = defaultState, action) => {
@@ -23,8 +24,8 @@ export const productsReducer = (state = defaultState, action) => {
                   ...item,
                   saleShow: true,
                   rangeShow: true,
-               })),
-            };
+               }))
+            }
          } else {
             return {
                page: { title: 'All Products' },
@@ -32,8 +33,8 @@ export const productsReducer = (state = defaultState, action) => {
                   ...item,
                   saleShow: true,
                   rangeShow: true,
-               })),
-            };
+               }))
+            }
          }
       
       case LOAD_PRODUCTS_SALE:
@@ -45,49 +46,72 @@ export const productsReducer = (state = defaultState, action) => {
          }
       
       case FILTER_SALE_PRODUCTS:
-         if (action.payload) {
-            return state.map(item => {
-               if (item.discont_price === null) {
-                  item.saleShow = false
-               }
-               return item
-            })
-         } else {
-            return state.map(item => ({ ...item, saleShow: true}))
+         return {
+            ...state,
+            list: state.list.map((item) => ({
+               ...item,
+               saleShow: action.payload
+                  ? item.discont_price ?? false
+                  : item.discont_price ?? true
+            }))
+         }
+
+      case FILTER_PRICE_RANGE:
+         const { from, to } = action.payload
+         return {
+            ...state,
+            list: state.list.map((item) => ({
+               ...item,
+               rangeShow:
+                  item.price >= +from &&
+                  (item.discont_price || item.price) <= +to
+            }))
          }
 
       case SORT_DEFAULT_PRODUCTS:
-         return [...state].sort((a, b) => a.id - b.id)
+         return {
+            ...state,
+            list: [...state.list].sort((a, b) => a.id - b.id)
+         }
       
-      case SORT_PRICE_DOWN_PRODUCTS:
-         return [...state].sort((a, b) =>
-            (b.discont_price ? b.discont_price : b.price) -
-            (a.discont_price ? a.discont_price : a.price))
+      case SORT_PRICE_DOWN:
+         return {
+            ...state,
+            list: [...state.list].sort((a, b) => {
+               const current = a.discont_price || a.price;
+               const next = b.discont_price || b.price;
+               return current === next ? 0 : current > next ? -1 : 1;
+            })
+         }
       
-      case SORT_PRICE_UP_PRODUCTS:
-         return [...state].sort((a, b) => {
-            const priceA = a.discont_price ? a.discont_price : a.price;
-            const priceB = b.discont_price ? b.discont_price : b.price;
-            return priceA - priceB
-         })
+      case SORT_PRICE_UP:
+         return {
+            ...state,
+            list: [...state.list].sort((a, b) => {
+               const prev = a.discont_price || a.price;
+               const actual = b.discont_price || b.price;
+               return prev === actual ? 0 : prev > actual ? 1 : -1;
+         
+            })
+         }   
+      
       
       case SORT_NAME_PRODUCTS:
-         return [...state].sort((a, b) => a.title.localeCompare(b.title))
+         return {
+            ...state,
+            list: [...state.list].sort((a, b) => {
+               const titleA = a.title.toUpperCase();
+               const titleB = b.title.toUpperCase();
+               return titleA === titleB ? 0 : titleA > titleB ? 1 : -1;
+            }),
+         };
+
+         // return {
+         //    ...state,
+         //    list: [...state.list].sort((a, b) => a.title.localeCompare(b.title))
+         // }
+   
       
-      case FILTER_RANGE_PRODUCTS:
-         let { from, to } = action.payload
-         if (isNaN(from)) {
-            from = -Infinity
-         }
-         if (isNaN(to)) {
-            to = Infinity
-         }
-         return [...state].map(item => ({
-            ...item,
-            rangeShow:
-               (item.discont_price ? item.discont_price : item.price) >= from
-               && (item.discont_price ? item.discont_price : item.price) <= to
-         }))
 
       default:
          return state
@@ -97,8 +121,10 @@ export const productsReducer = (state = defaultState, action) => {
 export const loadProductsAction = (payload) => ({ type: LOAD_PRODUCTS, payload })
 export const loadProductsSaleAction = () => ({ type: LOAD_PRODUCTS_SALE })
 export const filterSaleProductsAction = (payload) => ({ type: FILTER_SALE_PRODUCTS, payload })
+export const filterRangeProductsAction = (payload) => ({ type: FILTER_PRICE_RANGE, payload })
+
 export const sortDefaultProductsAction = () => ({ type: SORT_DEFAULT_PRODUCTS })
-export const sortPriceDownProductsAction = () => ({ type: SORT_PRICE_DOWN_PRODUCTS })
-export const sortPriceUpProductsAction = () => ({ type: SORT_PRICE_UP_PRODUCTS })
+export const sortPriceDownProductsAction = () => ({ type: SORT_PRICE_DOWN })
+export const sortPriceUpProductsAction = () => ({ type: SORT_PRICE_UP })
 export const sortNameProductsAction = () => ({ type: SORT_NAME_PRODUCTS })
-export const filterRangeProductsAction = (payload) => ({ type: FILTER_RANGE_PRODUCTS, payload })
+
